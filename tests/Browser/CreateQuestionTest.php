eb5e75c711d046e3f96db03de5d6b323da939798
pooja <pk537@njit.edu>
2018-12-15 16:50:47 -0500

    Task: Test to view question and delete question are passed

--- tests/Browser/CreateQuestionTest.php
+++ tests/Browser/CreateQuestionTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser;
 
+use phpDocumentor\Reflection\Types\Integer;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -15,19 +16,23 @@ class CreateQuestionTest extends DuskTestCase
      * @return void
      */
     use DatabaseMigrations;
+
     public function testCreateQuestion()
     {
+
         factory(User::class)->create();
         $this->browse(function (Browser $browser) {
             $browser->loginAs(User::find(1))
                     ->visit('/question/create')
                     ->assertSee('Create Question')
-                    ->type('body','ZZZZZZZZZZZZ')
+                    ->type('body', 'what is laravel?')
                     ->press('Save')
                     ->assertPathIs('/home')
                     ->assertSee('IT WORKS!');
-            ;
+
 
         });
+        //$id = DB::table('questions')->select('id')->where('body', $body)->first();
+
     }
 }
